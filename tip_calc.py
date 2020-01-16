2cb806df89003d6c7f03f53d9a83d5f25704798d
Patrick Furtak <patrick@patrickfurtak.com>
2020-01-16 15:57:13 -0500

    Added cost per person functionality

--- tip_calc.py
+++ tip_calc.py
@@ -3,6 +3,7 @@
 # prompt for total bill amount and service level
 bill_total = float(input('What is the bill amount? '))
 service_level = input('How was the level of service? (good, fair or bad) ')
+split = float(input('How many people will be paying for this meal? '))
 
 if service_level == "good":
     tip = bill_total * .20
@@ -12,11 +13,8 @@ else:
     tip = bill_total * .10
 
 bill_with_tip = tip + bill_total
+total_per_person = bill_with_tip / split
 
 print("Tip amount: $" + '%.2f' % tip)
 print("Total amount: $" + '%.2f' % bill_with_tip)
-
-
-###########################################################
-# REVISIT TO ADD GROUP ORDER CALCULATION
-###########################################################
+print("Total amount per person: $" + '%.2f' % total_per_person)
