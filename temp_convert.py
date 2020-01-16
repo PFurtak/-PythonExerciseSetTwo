35ac603a7fc3276a4bd4d51e9fa90b91739f830d
Patrick Furtak <patrick@patrickfurtak.com>
2020-01-16 12:12:52 -0500

    Day 1 Python exercise set 2

---
+++ temp_convert.py
@@ -0,0 +1,8 @@
+# Celsius to Fahrenheit conversion calculator
+
+# prompt user for temperature in Celsius
+c = int(input("Temperature in C? "))
+# convert to Fahrenheit
+f = int((c * 1.8) + 32)
+# display result
+print(f, "F")
